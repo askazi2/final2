cebaa1cd3e4572988c873263fe65d655b5917968
annaedat <aedat2@illinois.edu>
2019-12-02 18:17:30 -0600

    Initial Check

--- app/src/main/java/com/example/finalproject125/MainActivity.java
+++ app/src/main/java/com/example/finalproject125/MainActivity.java
@@ -3,13 +3,31 @@ package com.example.finalproject125;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button shuffle;
+    private TextView saySome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        saySome = findViewById(R.id.saySome);
+        saySome.setVisibility(View.GONE);
+
+        shuffle = findViewById(R.id.shuffleAnything);
+        shuffle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                generateEpisode();
+            }
+        });
     }
-    // new comment
+
+    public void generateEpisode() {
+        saySome.setVisibility(View.VISIBLE);
+    }
 }
