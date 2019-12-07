0af1398b747c4cbbd2e0303412d59acdd7246022
ayesh <askazi2@illinois.edu>
2019-12-06 19:38:57 -0600

    editing code to make it work for our app

---
+++ app/src/main/java/com/example/finalproject125/EpisodeName.java
@@ -0,0 +1,11 @@
+package com.example.finalproject125;
+
+public class EpisodeName {
+    private String title;
+    public EpisodeName(String var1) {
+        title = var1;
+    }
+    private String getTitle() {
+        return title;
+    }
+}
--- app/src/main/java/com/example/finalproject125/NewActivity.java
+++ app/src/main/java/com/example/finalproject125/NewActivity.java
@@ -1,5 +1,195 @@
 package com.example.finalproject125;
 
-public class NewActivity {
-    //hi
+import android.content.Context;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class NewActivity extends MainActivity {
+    private LinearLayout llContainer;
+    private EditText etSearch;
+    private ListView lvProducts;
+
+    private ArrayList<EpisodeName> mProductArrayList = new ArrayList<EpisodeName>();
+    private MyAdapter adapter1;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initialize();
+
+
+        // Add Text Change Listener to EditText
+        etSearch.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Call back the Adapter with current character to Filter
+                adapter1.getFilter().filter(s.toString());
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+    private void initialize() {
+        // need to add to xml file
+        etSearch = (EditText) findViewById(R.id.etSearch);
+        lvProducts = (ListView)findViewById(R.id.lvOS);
+    }
+
+    @Override
+    protected void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+
+        // this is how they added the string and number
+        // mProductArrayList.add(new Product("a", 100));
+
+        // this should work when you put it back into MainActivity
+        adapter1 = new MyAdapter(MainActivity.this, mProductArrayList);
+        lvProducts.setAdapter(adapter1);
+    }
+
+
+    // Adapter Class
+    public class MyAdapter extends BaseAdapter implements Filterable {
+
+        private ArrayList<EpisodeName> mOriginalValues; // Original Values
+        private ArrayList<EpisodeName> mDisplayedValues;    // Values to be displayed
+        LayoutInflater inflater;
+
+        public MyAdapter(Context context, ArrayList<EpisodeName> mProductArrayList) {
+            this.mOriginalValues = mProductArrayList;
+            this.mDisplayedValues = mProductArrayList;
+            inflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public int getCount() {
+            return mDisplayedValues.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        private class ViewHolder {
+            LinearLayout llContainer;
+            TextView tvName,tvPrice;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder = null;
+
+            if (convertView == null) {
+
+                holder = new ViewHolder();
+                // need to add to linear layout a row, container, name, and price
+                // edit to what we actually need
+                convertView = inflater.inflate(R.layout.row, null);
+                holder.llContainer = (LinearLayout)convertView.findViewById(R.id.llContainer);
+                holder.tvName = (TextView) convertView.findViewById(R.id.tvName);
+                holder.tvPrice = (TextView) convertView.findViewById(R.id.tvPrice);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+            holder.tvName.setText(mDisplayedValues.get(position).name);
+            holder.tvPrice.setText(mDisplayedValues.get(position).price+"");
+
+            // I edited this ... don't know if it works
+            holder.llContainer.setOnClickListener(new View.OnClickListener() {
+
+                public void onClick(View v) {
+
+                    Toast.makeText(MainActivity.this, mDisplayedValues.get(position).name, Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            return convertView;
+        }
+
+        @Override
+        public Filter getFilter() {
+            Filter filter = new Filter() {
+
+                @SuppressWarnings("unchecked")
+                @Override
+                protected void publishResults(CharSequence constraint,FilterResults results) {
+
+                    mDisplayedValues = (ArrayList<EpisodeName>) results.values; // has the filtered values
+                    notifyDataSetChanged();  // notifies the data with new filtered values
+                }
+
+                @Override
+                protected FilterResults performFiltering(CharSequence constraint) {
+                    FilterResults results = new FilterResults();        // Holds the results of a filtering operation in values
+                    ArrayList<EpisodeName> FilteredArrList = new ArrayList<EpisodeName>();
+
+                    if (mOriginalValues == null) {
+                        mOriginalValues = new ArrayList<EpisodeName>(mDisplayedValues); // saves the original data in mOriginalValues
+                    }
+
+                    /********
+                     *
+                     *  If constraint(CharSequence that is received) is null returns the mOriginalValues(Original) values
+                     *  else does the Filtering and returns FilteredArrList(Filtered)
+                     *
+                     ********/
+                    if (constraint == null || constraint.length() == 0) {
+
+                        // set the Original result to return
+                        results.count = mOriginalValues.size();
+                        results.values = mOriginalValues;
+                    } else {
+                        constraint = constraint.toString().toLowerCase();
+                        for (int i = 0; i < mOriginalValues.size(); i++) {
+                            String data = mOriginalValues.get(i).name;
+                            if (data.toLowerCase().startsWith(constraint.toString())) {
+                                // need to edit this because our class only takes in a string as a parameter
+                                FilteredArrList.add(new EpisodeName(mOriginalValues.get(i).name,mOriginalValues.get(i).price));
+                            }
+                        }
+                        // set the Filtered result to return
+                        results.count = FilteredArrList.size();
+                        results.values = FilteredArrList;
+                    }
+                    return results;
+                }
+            };
+            return filter;
+        }
+    }
+
 }
