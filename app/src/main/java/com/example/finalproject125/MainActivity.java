2f5b45275a3746dfe3c12ef4ee18d1d74709272d
annaedat <aedat2@illinois.edu>
2019-12-07 16:03:28 -0600

    New Stuff

--- app/src/main/java/com/example/finalproject125/MainActivity.java
+++ app/src/main/java/com/example/finalproject125/MainActivity.java
@@ -1,49 +1,41 @@
 package com.example.finalproject125;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Filter;
-import android.widget.Filterable;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONObject;
+
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
 
 public class MainActivity extends AppCompatActivity {
     // this code is for the button that we created
-    private Button shuffle;
-    private TextView saySome;
+    private Button submit;
+    private String title;
+    private EditText titleInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        saySome = findViewById(R.id.saySome);
-        saySome.setVisibility(View.GONE);
 
-        shuffle = findViewById(R.id.shuffleAnything);
-        shuffle.setOnClickListener(new View.OnClickListener() {
+        submit = findViewById(R.id.submit);
+        titleInput = findViewById(R.id.titleInput);
+        submit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                generateEpisode();
+            public void onClick(View v) {
+                title = titleInput.getText().toString();
+                HttpResponse<String> response = Unirest.get("https://movie-database-imdb-alternative.p.rapidapi.com/?page=1&r=json&s=Frozen")
+                        .header("x-rapidapi-host", "movie-database-imdb-alternative.p.rapidapi.com")
+                        .header("x-rapidapi-key", "3bda5a528bmsh698ae8b83d27e37p165ee0jsnec6e42c11bc3")
+                        .asString();
+                JSONObject myResponse = new JSONObject(response);
             }
         });
     }
 
-    public void generateEpisode() {
-        // this makes the button actually show up as visible
-        saySome.setVisibility(View.VISIBLE);
-    }
 }
