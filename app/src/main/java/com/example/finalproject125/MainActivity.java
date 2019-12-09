e2a919f4d335243fec2b9decb509b9847705a644
ayesh <askazi2@illinois.edu>
2019-12-09 17:36:20 -0600

    we doneeeeee that

--- app/src/main/java/com/example/finalproject125/MainActivity.java
+++ app/src/main/java/com/example/finalproject125/MainActivity.java
@@ -70,19 +70,20 @@ public class MainActivity extends AppCompatActivity {
         protected String doInBackground(String... params) {
             // Do some validation here
             String newString = "";
-            for (int i = 0; i < title.length() - 1; i++) {
+            for (int i = 0; i < title.length(); i++) {
                 if (title.substring(i, i + 1).equals(" ")) {
                     newString += "%20";
                 } else {
                     newString += title.substring(i, i + 1);
                 }
             }
+            System.out.println(newString);
 
             OkHttpClient client = new OkHttpClient();
 
             try {
                 Request request = new Request.Builder()
-                        .url("https://movie-database-imdb-alternative.p.rapidapi.com/?page=1&r=json&s=Avengers%20Endgame")
+                        .url("https://movie-database-imdb-alternative.p.rapidapi.com/?page=1&r=json&s=" + newString)
                         .get()
                         .addHeader("x-rapidapi-host", "movie-database-imdb-alternative.p.rapidapi.com")
                         .addHeader("x-rapidapi-key", "3bda5a528bmsh698ae8b83d27e37p165ee0jsnec6e42c11bc3")
--- app/src/main/java/com/example/finalproject125/MovieInfo.java
+++ app/src/main/java/com/example/finalproject125/MovieInfo.java
@@ -1,10 +1,12 @@
 package com.example.finalproject125;
 
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -21,15 +23,25 @@ import org.json.JSONObject;
 public class MovieInfo extends AppCompatActivity {
     private String response;
     private TextView responseView;
+    private Button newMovie;
     @Override
     protected void onCreate(Bundle here) {
         super.onCreate(here);
         setContentView(R.layout.movie_info);
         response = getIntent().getStringExtra("response");
         responseView = findViewById(R.id.responseView2);
+        newMovie = findViewById(R.id.newmovie);
         responseView.setText("");
         responseView.setText(response);
         responseView.setVisibility(View.VISIBLE);
+        newMovie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent i = new Intent(this, MainActivity.class);
+                //startActivity(i);
+                finish();
+            }
+        });
     }
 
 }
